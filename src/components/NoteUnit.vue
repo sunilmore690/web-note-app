26364e3e02f7a648f2675f9c371e6f3cfb184f5a
Sunil More <sunilmore690@gmail.com>
2023-03-31 13:01:26 +0530

    format date time

--- src/components/NoteUnit.vue
+++ src/components/NoteUnit.vue
@@ -1,5 +1,9 @@
 <template>
-  <div @mouseover="close = true" @mouseleave="close = false" style="margin-bottom:  8px">
+  <div
+    @mouseover="close = true"
+    @mouseleave="close = false"
+    style="margin-bottom: 8px"
+  >
     <el-card
       class="box-card note-list-unit"
       :class="{ active: note.id === noteobj.id }"
@@ -28,8 +32,8 @@
       </div>
       <span style="font-weight: bold">{{ noteobj.title | trim(40) }}</span>
       <el-row style="font-size: smaller; padding-top: 10px">
-        <el-col :span="12">Created :{{ noteobj.created | formatDate }}</el-col>
-        <el-col :span="12">Updated : {{ noteobj.updated | formatDate }}</el-col>
+        <el-col :span="12">Created :{{ noteobj.created | updatedFormatDate }}</el-col>
+        <el-col :span="12">Updated : {{ noteobj.updated | updatedFormatDate }}</el-col>
       </el-row>
     </el-card>
   </div>
@@ -54,6 +58,27 @@ export default {
         return value;
       }
       return value.slice(0, count) + "...";
+    },
+    updatedFormatDate(dateString) {
+      const date = new Date(dateString);
+      const now = new Date();
+      const diff = (now.getTime() - date.getTime()) / 1000;
+
+      if (diff < 60) {
+        return Math.floor(diff) + " seconds ago";
+      } else if (diff < 60 * 60) {
+        const minutes = Math.floor(diff / 60);
+        return minutes === 1 ? "1 minute ago" : minutes + " minutes ago";
+      } else if (diff < 60 * 60 * 24) {
+        const hours = Math.floor(diff / (60 * 60));
+        return hours === 1 ? "1 hour ago" : hours + " hours ago";
+      } else if (diff < 60 * 60 * 24 * 5) {
+        const days = Math.floor(diff / (60 * 60 * 24));
+        return days === 1 ? "1 day ago" : days + " days ago";
+      } else {
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        return date.toLocaleDateString("en-US", options);
+      }
     },
     formatDate(date) {
       try {
